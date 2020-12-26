4fd71ac1bb1ee0c0f266f6b6efe19642efdc7ed7
minhquan0902 <minhquan0902@gmail.com>
2020-12-26 17:51:41 +0800

    Login mutation with graphql

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
-  parser: babel - eslint,
+  parser: "babel-eslint",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
