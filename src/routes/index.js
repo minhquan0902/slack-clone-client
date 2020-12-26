fba66700a60176f5f4a6e7538eaf7bfc45fe78c5
minhquan0902 <minhquan0902@gmail.com>
2020-12-27 04:35:31 +0800

    Verifying, Refreshing JWT Tokens and Prottected Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,51 @@
-/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import decode from "jwt-decode";
+
 import Home from "./Home";
 import Register from "./register";
 import Login from "./Login";
-// eslint-disable-next-line react/display-name
+import CreateTeam from "./CreateTeam";
+
+const isAuthenticated = () => {
+  const token = localStorage.getItem("token");
+  const refreshToken = localStorage.getItem("refreshToken");
+  try {
+    decode(token);
+    decode(refreshToken);
+  } catch (err) {
+    return false;
+  }
+
+  return true;
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+          }}
+        />
+      )
+    }
+  />
+);
+
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <PrivateRoute path="/create-team" exact component={CreateTeam} />
     </Switch>
   </BrowserRouter>
 );
